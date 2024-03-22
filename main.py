baeb596cc9b96ef60a0d1f4f7c3f2429c17c27a3
mwangi-george <mwangigeorge648@gmail.com>
2024-03-22 07:09:49 +0300

    updates

---
+++ main.py
@@ -0,0 +1,46 @@
+text = "programming is amazing   "
+
+
+print(text.index("z"))
+
+
+print(text[0:10])
+
+print(text.upper())
+
+print(text.capitalize())
+
+print(text.casefold())
+
+print(text.center(2))
+print(text.count("g"))
+
+print(text.join("hhd"))
+
+print(text.split("  ---   "))
+
+print(text*3)
+
+print(text.strip())
+
+my_num = "2"
+
+print(int(my_num))
+
+x = 133 % 100
+print(x)
+
+y = ""
+print(type(y))
+
+l_1 = [1, 23, 54]
+
+l_2 = ["a", "d"]
+
+print(l_1 + l_2)
+
+print(l_1.pop())
+
+tp = (1, 2, 3)
+tp[1] = 3
+print(tp)
