15dbd8136087018fea25e2bdc2b98cb6c7d714cd
mwangi-george <mwangigeorge648@gmail.com>
2024-04-19 12:21:00 +0300

    get and setdefault methods

--- main.py
+++ main.py
@@ -57,3 +57,15 @@ print(my_dict)
 my_dict["nested_dict"] = {1: "a", 2: "b"}
 
 print(my_dict)
+
+
+# accessing values with get method
+print(my_dict.get("nested_dict"))
+
+# setting a default value
+my_dict.setdefault("my_name", {1: 3, 4: 6})
+print(my_dict)
+
+# if the value alrady exists, setdefault has no effect
+my_dict.setdefault("my_name", "GKM")
+print(my_dict)
