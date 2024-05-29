97a15cd194e56b56549df84d2c0b1f080349eb01
mwangi-george <mwangigeorge648@gmail.com>
2024-05-29 16:40:36 +0300

    context manager

--- file_handling.py
+++ file_handling.py
@@ -1,24 +1,45 @@
-# creating a directory
-import os
-if not os.path.exists("files"):
-    os.mkdir("files")
-else:
-    # opening a file in write mode (used when we know the file does not exist)
-    # Overwrites content
-    file_text = open("files/test_file.txt", "w")
-    # Open in append mode (When the file already exist)
-    file_csv = open("files/test_file.csv", "a")
-    file_xls = open("files/test_file.xls", "w")
-    file_tsv = open("files/test_file.tsv", "w")
+# # creating a directory
+# import os
+# if not os.path.exists("files"):
+#     os.mkdir("files")
+# else:
+#     # opening a file in write mode (used when we know the file does not exist)
+#     # Overwrites content
+#     file_text = open("files/test_file.txt", "w")
+#     file_text.write("this is our first line \n")
+#     file_text.close()
 
-    file_text.write("this is our first line\n")
-    file_text.write("this is our second line")
+#     # Open in append mode (When the file already exist)
+#     file_text = open("files/test_file.txt", "a")
+#     file_text.write("this is our second line (file handling)\n")
+#     file_text.write("this is our third line (file handling)\n")
+#     file_text.write("this is our fourth line (file handling)\n")
+#     file_text.write("this is our fifth line (file handling)\n")
+#     file_text.close()
 
-    file_csv.write("name,age,George,23")
+#     file_csv = open("files/test_file.csv", "w")
+#     file_csv.write("name,age,George,23")
+#     file_csv.close()
 
-    # close connection to files
-    file_text.close()
-    file_csv.close()
-    file_xls.close()
-    file_tsv.close()
-    print("Operation successful")
+#     # open file in read mode
+#     file_text = open("files/test_file.txt", "r")
+#     file_content = file_text.read()
+#     print(file_content)
+
+#     file_line = file_text.readline()
+#     print(file_line)
+
+#     for line in file_text.readlines():
+#         print(line)
+
+#     file_text.close()
+
+# print("Operation successful")
+
+# Using context manager (we dont have to close file)
+with open("files/test_file.txt", "r") as f:
+    content = f.read()
+    print(content)
+
+    for line in f.readlines():
+        print(line)
