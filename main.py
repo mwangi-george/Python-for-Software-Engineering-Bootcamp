be781bab3be561e6cc13b2c3adfb6dd34d747168
mwangi-george <mwangigeorge648@gmail.com>
2024-05-29 21:51:56 +0300

    Classes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from classes import Animal
+from classes import Animal, Person
 app = FastAPI()
 
 
@@ -7,3 +7,10 @@ app = FastAPI()
 def home():
     dog = Animal(12, 30)
     return dog.print_height()
+
+
+@app.get("/user")
+def user_info():
+    user = Person("George", "English", "Kenya", "Software Engineer")
+    lang, *_, work = user.person_language(), user.person_nationality(), user.person_profession()
+    return lang, work
