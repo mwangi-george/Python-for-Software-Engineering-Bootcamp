15571da5d1a3b50feb47cf1ae89ef256c890e267
mwangi-george <mwangigeorge648@gmail.com>
2024-06-07 13:39:23 +0300

    Classes

--- test_classes.py
+++ test_classes.py
@@ -10,4 +10,5 @@ print(nairobi_workshop.workshop_participants())
 
 print(nairobi_workshop.countries)
 
+# Accessing attributes directly without instanciating the class
 print(NtdModelingWorkshop.next_host())
