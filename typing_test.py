c8e1521f5789c0aab587962a190f1d02bd6c460d
George Mwangi <mwangigeorge648@gmail.com>
2024-06-21 13:03:56 +0300

    typing

--- typing_test.py
+++ typing_test.py
@@ -47,3 +47,24 @@ def division(a: int, b: int) -> Optional[float]:
     """
     if b != 0:
         return a / b
+
+
+def division(a: int, b: Optional[int]) -> Union[int, float, None]:
+    """
+    Optional inputs
+
+    Complicated outputs
+    """
+    if b is None:
+        return a
+    if b != 0:
+        return a / b
+    return None
+
+
+def test_unpack(input: dict[str, int], dict_key: str) -> int:
+    return input[dict_key]
+
+
+return_val = test_unpack({"some_val": 34, "val2": 36}, "val2")
+print(return_val)
