3d58543f291aa5759556f11f2964ce4c7eee0839
George Mwangi <mwangigeorge648@gmail.com>
2024-06-29 16:13:15 +0300

    Path Parameters

--- api2.py
+++ api2.py
@@ -79,3 +79,53 @@ def get_road_info(road_id) -> Roads:
 @app.get("/roads/{road_id}", response_model=Roads)
 def get_road_by_id(road_id: int) -> dict:
     return get_road_info(road_id)
+
+
+# More than one path parameters
+
+class Name(BaseModel):
+    id: int
+    first_name: str
+    middle_name: Optional[str] = None
+    last_name: str
+
+    # Attributes Configurations
+    class Config:
+        str_max_length = 30
+        str_min_length = 2
+
+
+def get_name(name_id: str = "default") -> Name:
+    # Using a list of dictionaries for easier subsetting using the index
+    all_names = {
+        "default": {
+            "id": 0,
+            "first_name": "George",
+            "last_name": "Mwangi"
+        },
+        "user_1": {
+            "id": 1,
+            "first_name": "Alex",
+            "middle_name": "Ross",
+            "last_name": "McRuger"
+        },
+        "user_2": {
+            "id": 2,
+            "first_name": "James",
+            "last_name": "Bond"
+        },
+        "user_3": {
+            "id": 3,
+            "first_name": "Michael",
+            "middle_name": "McGovern",
+            "last_name": "John II"
+        }
+    }
+
+    person_name = all_names[name_id]
+    return Name(**person_name)
+
+
+@app.get("/names/{name_id}/{origin}", response_model=Name)
+def get_name_by_id(name_id: str, origin: str) -> dict:
+    return get_name(name_id)
