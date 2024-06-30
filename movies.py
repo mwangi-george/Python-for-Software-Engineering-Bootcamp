513349f25603aec80908ef712d1b04c2b720d13b
George Mwangi <mwangigeorge648@gmail.com>
2024-06-30 22:39:25 +0300

    Updates

--- movies.py
+++ movies.py
@@ -24,6 +24,10 @@ class Movie(BaseModel):
         str_min_length = 4
 
 
+class MultipleMoviesResponse(BaseModel):
+    movies: list[Movie]
+
+
 all_movies = {
     0: {
         "title": "My journey in Software Engineering",
@@ -52,7 +56,26 @@ def new_movie(movie_details: Movie) -> None:
     }
     return None
 
-# Create a Movie
+
+def get_movie(movie_id: int = 0) -> Movie:
+    movie = all_movies[movie_id]
+    return Movie(**movie)
+
+
+def get_multiple_movies_paginated(start: int = 0, limit: int = 20):
+
+    list_of_movies = []
+    movie_dict_keys = list(all_movies.keys())
+
+    for index in range(0, len(movie_dict_keys), 1):
+        if index < start:
+            continue
+        current_key = movie_dict_keys[index]
+        movie = get_movie(current_key)
+        list_of_movies.append(movie)
+        if len(list_of_movies) >= limit:
+            break
+    return list_of_movies
 
 
 @app.post("/movies")
@@ -62,14 +85,6 @@ def create_movie(movie_details: Movie) -> None:
     return None
 
 
-def get_movie(movie_id: int = 0) -> Movie:
-    movie = all_movies[movie_id]
-    return Movie(**movie)
-
-
-# Get Movie details by id
-
-
 @app.get("/movie/{movie_id}", response_model=Movie)
 def get_movie_by_id(movie_id: int) -> Movie:
     """Endpoint to get movie details by id"""
@@ -77,3 +92,8 @@ def get_movie_by_id(movie_id: int) -> Movie:
 
 
 # Get multiple movies details (query parameters)
+@app.get("/movies", response_model=MultipleMoviesResponse)
+def get_multiple_movies(start: int = 0, limit: int = 10) -> MultipleMoviesResponse:
+    movies = get_multiple_movies_paginated(start, limit)
+    formatted_movies = MultipleMoviesResponse(movies=movies)
+    return formatted_movies
