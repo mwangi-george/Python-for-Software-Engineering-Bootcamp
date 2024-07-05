31003e72883a237bbd5e4e3b6aaf35f62b87832a
George Mwangi <mwangigeorge648@gmail.com>
2024-07-05 18:25:59 +0300

    Creating and Deleting Schemas

--- test_file.sql
+++ test_file.sql
@@ -26,3 +26,19 @@ CREATE TABLE IF NOT EXISTS event_log (
     FOREIGN KEY (user_id) REFERENCES user_info (id),
     FOREIGN KEY (event_id) REFERENCES event_info (id)
 );
+
+-- Specifying schema during table creation (Great when dealing with multiple schemas)
+CREATE TABLE IF NOT EXISTS public.event_info (
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(25)
+);
+
+-- Deleting the created table
+DROP TABLE public.event_info;
+
+-- Deleting a schema.
+DROP SCHEMA my_tables_schema; -- This will cause an error if there are tables inside
+
+-- To clean dependencies inside the schema
+DROP SCHEMA my_tables_schema CASCADE;
+
